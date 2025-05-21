512ae4662fba8c0ae4211b4b0f2e89136f2efc92
Luis <luisouza.gu10@gmail.com>
2025-05-21 15:33:25 -0300

    feat: Creating findAll from loading-list.

--- src/modules/loading-list/loading-list/loading-list.service.ts
+++ src/modules/loading-list/loading-list/loading-list.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateLoadingListDto } from './dto/create-loading-list.dto';
 import { UpdateLoadingListDto } from './dto/update-loading-list.dto';
+import { LoadingList } from 'src/entities/loadinglist.entities';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class LoadingListService {
+  constructor(
+      @InjectRepository(LoadingList)
+      private readonly loadinglistRepository: Repository<LoadingList>,
+  ) {}
   create(createLoadingListDto: CreateLoadingListDto) {
     return 'This action adds a new loadingList';
   }
 
-  findAll() {
-    return `This action returns all loadingList`;
+  async findAll() {
+    const list = this.loadinglistRepository.find({
+      where: {
+        isActive: true,
+      }
+    });
+    return list;
   }
-
   findOne(id: number) {
     return `This action returns a #${id} loadingList`;
   }
